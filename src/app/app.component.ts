68fc762d30336b597b3c65875d729a8570886b24
PavelAC <pavelandreicalin@gmail.com>
2024-12-04 13:32:10 +0200

    comuted to modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,11 @@
-import { Component } from '@angular/core';
-
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import type { InvestmentInput } from './investment-input-model';
-import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
-
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
+
 export class AppComponent {
-  dataResults?:{
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[];
   
-  calculateInvestmentResults(
-    data: InvestmentInput) {
-    const {initialInvestment,annualInvestment,duration,expectedReturn} = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-  
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-  
-    this.dataResults = annualData;
-  }
 }
 
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,29 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { InvestmentInput } from '../investment-input-model';
+import { Component, output, signal } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+// import { InvestmentInput } from '../investment-input-model';
 
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<InvestmentInput>();
+  // calculate = output<InvestmentInput>()
 
-  enteredInitailInvestment='0';
-  enteredAnnualInvestment='0';
-  enteredExpectedReturn='5';
-  enteredDuration='10';
+  enteredInitailInvestment=signal('0');
+  enteredAnnualInvestment=signal('0');
+  enteredExpectedReturn=signal('5');
+  enteredDuration=signal('10');
+
+  constructor(private investmentService: InvestmentService) {}
   
   onSubmit(){
-    this.calculate.emit({
-    initialInvestment: +this.enteredInitailInvestment,
-    duration: +this.enteredDuration, 
-    expectedReturn: +this.enteredExpectedReturn, 
-    annualInvestment: +this.enteredAnnualInvestment
-    });
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitailInvestment(),
+      duration: +this.enteredDuration(), 
+      expectedReturn: +this.enteredExpectedReturn(), 
+      annualInvestment: +this.enteredAnnualInvestment()
+      });
+
+    // this.calculate.emit();
+    // this.enteredInitailInvestment.set('0');
+    // this.enteredAnnualInvestment=signal('0');
+    // this.enteredExpectedReturn=signal('5');
+    // this.enteredDuration=signal('10');
   }
 }
